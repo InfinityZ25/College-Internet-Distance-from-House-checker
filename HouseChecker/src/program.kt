8608469c8cc7207d1ae476882be8cd981478da3f
Juan Cedeno <37850007+InfinityZ25@users.noreply.github.com>
2019-04-24 02:25:22 -0400

    Update program.kt

--- HouseChecker/src/program.kt
+++ HouseChecker/src/program.kt
@@ -2,16 +2,23 @@ import org.json.JSONObject
 
 class kotlinPart{
     val url = "https://www.att.com/services/shop/model/ecom/shop/view/unified/qualification/service/CheckAvailabilityRESTService/invokeCheckAvailability"
+    //This is my google api key. I Hope people don't steal it, whoops.
+    //I will add a settings file later so that everyone can easily change their key without having to recompile
     val google_api_key = "AIzaSyAoh11FSJlApTyPcOGyfRiX_jV5Pnir-dQ"
+    //This function gets called from Main in the java side of the program because I could not figure out a way to export a native Kotlin program
     fun run(address: String){
+        //Just here to indicate where the function starts
         println("Attempting to reach ATT's database")
-        //Input your data here
+        //Input your data here. This line is kinda pointless and can be simplified but I won't bother to fix it just yet.
         val addressToBeChecked = address
         val zip = addressToBeChecked.split(", ")[1]
-        //Program starts here
+        //Program starts here, get the time to calcuate the difference later.
         val initialTime = System.nanoTime()
+        //GET Request to AT&T's REST Service
         val attRestRequest = khttp.get(url,
             params = mapOf("userInputZip" to zip, "userInputAddressLine1" to addressToBeChecked, "mode" to "fullAddress"))
+        //GET Request to Google Maps' Matrix Distance API.
+        //UCF Can be changed for whatever college you want it to be.
         val googleRequest = gmapsQuery(addressToBeChecked, "UCF")
         //Check if the status of the response is ok
         if (attRestRequest.statusCode == 200){
@@ -24,6 +31,7 @@ class kotlinPart{
             if(highSpeed is String){
                 //Calculating how long does the request take to respond
                 val requestTime = (System.nanoTime() - initialTime) / 1000000000
+                //Send the user the data asked and calculated.
                 println("There is U-Verse ATT coverage available $addressToBeChecked" +
                         "\nMax available internet speed is: $highSpeed" +
                         "\nRequest took $requestTime seconds" +
@@ -33,18 +41,19 @@ class kotlinPart{
                         "\nDistance from UCF is ${googleMapsInquiry.getJSONObject("distance").getString("text")} or ${googleMapsInquiry.getJSONObject("duration").getString("text")}")
             }
         }else{
+            //I will handle this piece of the code better latter.
             println("Null request")
         }
     }
-
+    //This is a recursive function to get the JSON data from Google Maps' Matrix Distance API.
+    //The units argument can be set to metric, or be removed, if you wish.
+    //Adding the argument sensor=false makes the request more lightweight and optimizes the usage of the api by a lot.
     fun gmapsQuery(from: String, to: String): JSONObject {
-        val req = khttp.get(
+        return khttp.get(
             "https://maps.googleapis.com/maps/api/distancematrix/json?destinations=${to.replace(" ", "+")}" +
                     "&origins=${from.replace(" ", "+")}" +
                     "&units=imperial&sensor=false" +
                     "&key=$google_api_key"
-        )
-
-        return req.jsonObject
+        ).jsonObject
     }
 }
