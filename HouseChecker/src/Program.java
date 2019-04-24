f151bf95ee7cb27d3432f8ea0cdc3cf090e5d4a8
Juan Cedeno <37850007+InfinityZ25@users.noreply.github.com>
2019-04-24 02:29:35 -0400

    Update Program.java

--- HouseChecker/src/Program.java
+++ HouseChecker/src/Program.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
 
 public class Program {
+    //DISCLAIMER: I only utilized Koltin in this project because I like the language. This could've been easily done with plane Java
+    //            but it wouldn't have looked as pretty as with Kotlin. I probably will end up writting it in plane Java tbh. Just a
+    //            prove of concept of how much knowledge of kotlin do I have and how pretty it looks.
     public static void main(String[] args){
         //Just send a bunch of empty lines to clear the user's terminal.
+        //I might add a GUI and settings file later, for the time being, don't mind it.
         for (int i = 0; i < 100; i++) {
             System.out.println(" ");
         }
